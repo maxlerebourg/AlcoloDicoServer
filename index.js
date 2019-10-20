7b054b13d68cd7675e4260c80abe20238092e929
maxlerebourg <monecule2>
2019-10-20 22:52:52 +0200

    Revert "2 category added in the response"
    This reverts commit a8b85063

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ module.exports = [
                     },
                 }],
                 order: [[sequelize.literal('RANDOM()')]],
-                group: ['games->comments.gameId', 'categories.id', 'games.id', 'games->comments.gameId'],
+                group: ['games->comments.gameId', 'games.id'],
             });
             let news = await Game.findAll({
                 where: {[Op.or]: [{visible: false}, {updatedAt: {[Op.gte]:new Date().getTime() - 86400000 * 7}}]},
@@ -173,9 +173,8 @@ module.exports = [
                         [Comment.sequelize.fn('COUNT', Comment.sequelize.col('rate')), 'comments'],
                     ],
 
-
                 }],
-                group: ['games->comments.gameId', 'games.id', 'games->comments.gameId'],
+                group: ['games->comments.gameId', 'games.id'],
             });
             let two = await Game.findAll({
                 where: {multiplayer: 2},
@@ -189,7 +188,7 @@ module.exports = [
                     ],
 
                 }],
-                group: ['games->comments.gameId', 'games.id', 'games->comments.gameId'],
+                group: ['games->comments.gameId', 'games.id'],
             });
             return reply.response([{id: 100, name: 'Nouveautés', games: news}].concat(cat).concat({id: 101, name: 'Deux joueurs', games: two}));
         }
@@ -373,7 +372,7 @@ module.exports = [
                     ],
 
                 }],
-                group: ['games->comments.gameId', 'games.id',  'games->comments.gameId'],
+                group: ['games->comments.gameId', 'games.id'],
                 order: ['games.name']
             });
         }
